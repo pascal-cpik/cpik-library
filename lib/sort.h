803a5490631b89d5d6ce9751a3f5eaee3347cdd6
pascal-cpik <pascal+cpik@niklaus.org>
2015-03-08 21:34:08 +0100

    ssort working

--- lib/sort.h
+++ lib/sort.h
@@ -6,6 +6,7 @@
 typedef char (*sort_cmpfun)(void *e1, void *e2);
 void bsort(void *data, size_t length, unsigned char size, sort_cmpfun );
 void csort(void *data, size_t length, size_t size, sort_cmpfun);
+void ssort(void *data, size_t length, size_t size, sort_cmpfun);
 
 #endif // #ifndef __SORT_H
 
