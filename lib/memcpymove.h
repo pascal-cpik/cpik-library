7314a3a89e0221d81b6ab63a0d293e6def1577e2
pascal-cpik <pascal+cpik@niklaus.org>
2015-03-10 12:35:50 +0100

    changed int8_t to uint8_t in prototype of xmemshift

--- lib/memcpymove.h
+++ lib/memcpymove.h
@@ -5,6 +5,6 @@
 
 void *memcpy(void *dest, void *src, size_t num);
 void *memmove(void *dest, void *src, size_t num);
-void xmemshift(void *vec, uint8_t offset, int8_t shift, int8_t blklen, uint8_t size);
+void xmemshift(void *vec, uint8_t offset, int8_t shift, uint8_t blklen, uint8_t size);
 
 #endif
